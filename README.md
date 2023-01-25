85e92fdbb09ad2998b191956cc08d22061624cef
jayrussio10977 <120292190+jayrussio10977@users.noreply.github.com>
2023-01-24 22:49:29 -0500

    Coding Quiz feature update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Javascript_Coding_Quiz
+
+1. A coding quiz game that tests your basic knowledge on JavaScript
+
+2. Features a countdown timer and a variety of questions related toward Javascript.
+
+3. If questions are answered wrong game deducts 5 seconds off the countdown.
+
+4. Saves Name and Score to local storage once game is completed
--- assets/script.js
+++ assets/script.js
@@ -4,124 +4,150 @@ var buttons = document.querySelector(".btn-grid")
 var currentQuestionIndex = 0
 var question = document.querySelector(".question")
 var buttonParent = document.getElementById("answer-buttons")
+var timeLeft =document.getElementById("time-left")
 var current = {}
 var timerEl = 1000
-
+var time = 60
+var timerId;
+var score = 0
+var correctAnswers = 0
+var form =document.querySelector(".form")
+var nameInput = document.querySelector("#name")
+var emptyArray = []
 
 var questionsArray = [
     {
         num:1, 
         question: "Commonly used data types do Not include:",
-        answers: ["1.Strings","2.Booleans","3.Alerts","4.Numbers"], // answer choices
-        correct: "3.Alerts" // Correct answer
+        answers: ["1.Strings","2.Booleans","3.Alerts","4.Numbers"], 
+        correct: "3.Alerts" 
     },
     
     {
         num:2, 
         question: "The condition in an if/else statement is enclosed with ______.",
-        answers: ["1.quotes","2.curly brackets","3.parenthesis","4.square brackets"], // answer choices
-        correct: "3.parenthesis" // Correct answer
+        answers: ["1.quotes","2.curly brackets","3.parenthesis","4.square brackets"], 
+        correct: "3.parenthesis" 
     },
     
     {
         num:3, 
         question: "Arrays in JavaScript can be used ot store _____.",
-        answers: ["1.numbers and strings","2.booleans","3.other arrays","4.all of the above"], // answer choices
-        correct: "4.all of the above" // Correct answer
-     },
+        answers: ["1.numbers and strings","2.booleans","3.other arrays","4.all of the above"], 
+        correct: "4.all of the above" 
+    },
     
-     {
+    {
         num:4, 
         question: "String comments must be enclosed within _____ when being assigned to variables. ",
-        answers: ["1.commas","2.curly brackets","3.quotes","4.parenthesis"], // answer choices
-        correct: "3.quotes" // Correct answer
+        answers: ["1.commas","2.curly brackets","3.quotes","4.parenthesis"], 
+        correct: "3.quotes" 
     },
     
     {
         num:5, 
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["1.JavaScript","2.terminal/bash","3.for loops","4.console.log"], // answer choices
-        correct: "4.console.log" // Correct answer
-    },
-    
-  
-    
-    
-
+        answers: ["1.JavaScript","2.terminal/bash","3.for loops","4.console.log"], 
+        correct: "4.console.log" 
+    }
 ]
 
+form.setAttribute("hidden", true)
 
 function startQuiz() {
     control.setAttribute("hidden", true)
     container.removeAttribute("hidden")
-     getCurrentQuestion()
-    //  startTimer()
-
-
+    getCurrentQuestion()
+    timerId = setInterval(tickTimer, 1000)
+    
+    
 }
 
 function getCurrentQuestion() {
-    // console.log("Getting Question")
     var currentQuestion = questionsArray[currentQuestionIndex]
     current = currentQuestion
     for (let i = 0; i < currentQuestion.answers.length; i++) {
-        
         const answer = currentQuestion.answers[i];
         buttons.children[i].textContent = answer
         buttons.children[i].setAttribute("value", answer)
-        
     }
-// console.log(currentQuestion)
-    
-    // set the question txt in the index to the questions text in the loop
-    const questionText =document.getElementById('question')
+
+    const questionText = document.getElementById('question')
     questionText.innerHTML ="<h3> " + currentQuestion.question + "</h3>"
     currentQuestionIndex++
 }
 
-
 function handleSubmit(event) {
-    // console.log(event)
     event.preventDefault()
     if (event.target.value!== questionsArray[currentQuestionIndex].correct) {
         document.body.appendChild("Wrong Answer")
-    }   //figure out how to hook up the for answer buttons to this function!!
-
-
-
+    }  
+    
     if (event.target.value== questionsArray[currentQuestionIndex].correct){
-        // console.log(correct)
         getCurrentQuestion()
     }      
+    
+}
+
+function endQuiz() {
+    clearInterval(timerId)
+    form.removeAttribute("hidden")
 
 }
 
-//  function startTimer(time) {
-//     console.log("timer start")
-//     var time = questions.length * 15;
-// }
-
-
+function tickTimer() {
+    
+    time--;
+    timeLeft.innerHTML=time
+    
+    if(time == 0) {
+        endQuiz()
+       
+    }
+    
+}
 
 function optionSelection(event) {
-    // console.log(current)
-    if(currentQuestionIndex < questionsArray.length){
+    
+    var totalCorrectAnswers = questionsArray.length
+    
+    if (event.target.value.toLowerCase() === current.correct.toLowerCase()) { 
+        alert('correct')
         
-        console.log(event.target)
-        console.log(current)    
-            
+        correctAnswers++
         
+    } else { 
+        alert('not correct')
+        time-=5
         
-        
-        
-        getCurrentQuestion()
-
     }
+    
+    if (currentQuestionIndex < questionsArray.length) {
+        getCurrentQuestion()
+    } else{
+        alert(correctAnswers + "/" + totalCorrectAnswers)
+        
+    }
+}
+
+function submitForm(event) {
+    event.preventDefault()
+    localStorage.setItem("name", nameInput.value)
+    localStorage.setItem("correct-answers", correctAnswers)
+    localStorage.getItem()
 
 }
  
 
 control.addEventListener("click", startQuiz)
 buttonParent.addEventListener("click", optionSelection)
+form.addEventListener('submit',submitForm)
+
+
+
+
+
+
+
 
 
